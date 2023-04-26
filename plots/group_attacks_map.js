70df0896ca47e3b2605c7b7c8ae8a2ebe0af182b
AlexanderSternfeld <alex.sternfeld@live.nl>
2023-04-26 15:29:12 +0200

    debug map

--- plots/group_attacks_map.js
+++ plots/group_attacks_map.js
@@ -1,20 +1,17 @@
-// 1. Import D3
-import * as d3 from 'd3';
-
-// 2. Create an SVG container for the map
+// 1. Create an SVG container for the map
 const svg = d3.select('#group_map').append('svg')
     .attr('width', 800)
     .attr('height', 600);
 
-// 3. Define the map projection
+// 2. Define the map projection
 const projection = d3.geoMercator()
     .center([0, 0])
     .translate([400, 300])
     .scale(100);
 
-// 4. Load your dataset
+// 3. Load your dataset
 d3.json('data/group_attack_coordinates.json').then(data => {
-    // 5. Convert your coordinates into the format required by the map projection
+    // 4. Convert your coordinates into the format required by the map projection
     const geoData = data.map(d => ({
         type: 'Feature',
         geometry: {
@@ -26,11 +23,11 @@ d3.json('data/group_attack_coordinates.json').then(data => {
         },
     }));
 
-    // 6. Create a path generator that will create the path elements for your map
+    // 5. Create a path generator that will create the path elements for your map
     const path = d3.geoPath()
         .projection(projection);
 
-    // 7. Bind your data to path elements and append them to the SVG container
+    // 6. Bind your data to path elements and append them to the SVG container
     svg.selectAll('.point')
         .data(geoData)
         .enter()
@@ -38,7 +35,7 @@ d3.json('data/group_attack_coordinates.json').then(data => {
         .attr('class', 'point')
         .attr('d', path);
 
-    // 8. Style the path elements according to the terrorist group
+    // 7. Style the path elements according to the terrorist group
     svg.selectAll('.point')
         .style('fill', d => {
             switch (d.properties.group) {
@@ -55,7 +52,7 @@ d3.json('data/group_attack_coordinates.json').then(data => {
         })
         .style('opacity', 0.5);
 
-    // 9. Add interactivity to the map using D3's event listeners
+    // 8. Add interactivity to the map using D3's event listeners
     svg.selectAll('.point')
         .on('mouseover', function (d) {
             d3.select(this).style('stroke', 'black');
