0911b6368fc6a71a660b54af03aef2f18a196f6c
AGBonnet <abonnet04@gmail.com>
2023-05-04 17:26:57 +0200

    ssssss

--- plots/sunburst.js
+++ plots/sunburst.js
@@ -118,22 +118,22 @@ chart = async function (data) {
         (root);
     }
 
-    color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
 
-    format = d3.format(",d")
+format = d3.format(",d")
 
-    width = 932
+width = 932
 
-    radius = width / 6
+radius = width / 6
 
-    arc = d3.arc()
-    .startAngle(d => d.x0)
-    .endAngle(d => d.x1)
-    .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
-    .padRadius(radius * 1.5)
-    .innerRadius(d => d.y0 * radius)
-    .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
+arc = d3.arc()
+.startAngle(d => d.x0)
+.endAngle(d => d.x1)
+.padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+.padRadius(radius * 1.5)
+.innerRadius(d => d.y0 * radius)
+.outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
 
-    chart(id, data);
+chart(id, data);
 
 
