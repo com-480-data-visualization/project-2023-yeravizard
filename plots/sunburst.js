04db9778bcbe4f64687fc7b516ca90fb18639444
AGBonnet <abonnet04@gmail.com>
2023-05-04 17:21:49 +0200

    If this works I will scream

--- plots/sunburst.js
+++ plots/sunburst.js
@@ -1,4 +1,4 @@
-d3.csv("file.json", function (data) {
+chart = async function (data) {
     const root = partition(data);
   
     root.each(d => d.current = d);
@@ -105,4 +105,35 @@ d3.csv("file.json", function (data) {
     }
 
     return svg.node();
-    });
+    }
+
+    data = FileAttachment("../data/sunburst-tree.json").json();
+
+    partition = data => {
+      const root = d3.hierarchy(data)
+          .sum(d => d.value)
+          .sort((a, b) => b.value - a.value);
+      return d3.partition()
+          .size([2 * Math.PI, root.height + 1])
+        (root);
+    }
+
+    color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+
+    format = d3.format(",d")
+
+    width = 932
+
+    radius = width / 6
+
+    arc = d3.arc()
+    .startAngle(d => d.x0)
+    .endAngle(d => d.x1)
+    .padAngle(d => Math.min((d.x1 - d.x0) / 2, 0.005))
+    .padRadius(radius * 1.5)
+    .innerRadius(d => d.y0 * radius)
+    .outerRadius(d => Math.max(d.y0 * radius, d.y1 * radius - 1))
+
+    chart(id, data);
+
+
