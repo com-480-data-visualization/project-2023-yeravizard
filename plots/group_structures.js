9d195c1c9a717d0cd571f1c46b8fa1c8058c019d
AlexanderSternfeld <alex.sternfeld@live.nl>
2023-05-18 22:48:27 +0200

    grouped bar chart

--- plots/group_structures.js
+++ plots/group_structures.js
@@ -1,35 +1,32 @@
-const drawGroupStructures = () => {
-
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+const margin = { top: 10, right: 30, bottom: 30, left: 60 },
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg_structures = d3.select("#group_structures")
+const svg_structures = d3.select("#group_structures")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
 // Parse the Data
-d3.csv("../data/ideology_structure.csv", function (data) {
+d3.csv("data/ideology_structure.csv").then(function (data) {
 
-    // List of subgroups = header of the csv files = soil condition here
-    var subgroups = data.columns.slice(1)
+    // List of subgroups = header of the csv files
+    const subgroups = data.columns.slice(1)
 
     // List of groups = value of the first column called group -> I show them on the X axis
-    var groups = d3.map(data, function (d) { return (d.ideology) }).keys()
+    const groups = data.map(d => d.ideology)
 
     // Add X axis
-    var x = d3.scaleBand()
+    const x = d3.scaleBand()
         .domain(groups)
         .range([0, width])
         .padding([0.2])
     svg_structures.append("g")
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x).tickSize(0));
 
     // rotate x axis labels
@@ -38,53 +35,56 @@ d3.csv("../data/ideology_structure.csv", function (data) {
         .style("text-anchor", "end");
 
     // Add Y axis
-    var y = d3.scaleLog()
-        .domain([-5, 1])
+    const y = d3.scaleLog()
+        .domain([0.001, 1])
         .range([height, 0]);
     svg_structures.append("g")
         .call(d3.axisLeft(y));
 
     // Another scale for subgroup position?
-    var xSubgroup = d3.scaleBand()
+    const xSubgroup = d3.scaleBand()
         .domain(subgroups)
         .range([0, x.bandwidth()])
         .padding([0.05])
 
     // color palette = one color per subgroup
-    var color = d3.scaleOrdinal()
+    const color = d3.scaleOrdinal()
         .domain(subgroups)
-        .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00'])
+        .range(["#ffffb2", "#fecc5c", "#fd8d3c", "#f03b20", "#bd0026"]);
 
     // Show the bars
     svg_structures.append("g")
         .selectAll("g")
         // Enter in data = loop group per group
         .data(data)
-        .enter()
-        .append("g")
-            .attr("transform", function (d) { return "translate(" + x(d.ideology) + ",0)"; })
+        .join("g")
+            .attr("transform", d => `translate(${x(d.ideology)}, 0)`)
         .selectAll("rect")
         .data(function (d) { return subgroups.map(function (key) { return { key: key, value: d[key] }; }); })
-        .enter().append("rect")
-            .attr("x", function (d) { return xSubgroup(d.key); })
-            .attr("y", function (d) { return y(d.value); })
+        .join("rect")
+            .attr("x", d => xSubgroup(d.key))
+            .attr("y", d => y(d.value))
             .attr("width", xSubgroup.bandwidth())
-            .attr("height", function (d) { return height - y(d.value); })
-            .attr("fill", function (d) { return color(d.key); });
+            .attr("height", d => height - y(d.value))
+            .attr("fill", d => color(d.key));
     
     svg_structures.append("text")
         .attr("class", "y label")
         // size of text
         .attr("font-size", "14px")
+        // color white
+        .attr("fill", "white")
         .attr("text-anchor", "end")
         .attr("y", 6)
         .attr("dy", "-1.1em")
         .attr("transform", "rotate(-90)")
         .text("Proportion of groups with the indicated structure");
 
-    var legend = svg_structures.append("g")
+     legend = svg_structures.append("g")
         .attr("class", "legend")
         .attr("transform", "translate(" + (width + margin.right - 100) + "," + margin.top + ")")
+        // color white
+        .attr("fill", "white")
         .selectAll("g")
         .data(color.domain().slice().reverse())
         .enter().append("g")
@@ -104,7 +104,3 @@ d3.csv("../data/ideology_structure.csv", function (data) {
         .text(function (d) { return d; });
 
 })
-
-}
-
-export default drawGroupStructures;
