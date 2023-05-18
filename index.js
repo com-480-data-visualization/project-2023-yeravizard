12481582581ca6d4d8c0dcdba9d85a5c6b89ec89
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-05-18 10:27:57 +0200

    aaa

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import drawAreaChart from '/plots/area_chart.js';
-import drawSunburst from '/plots/sunburst.js';
-import drawGroupStructures from '/plots/group_structures.js';
-import drawRecruitment from '/plots/recruitment.js';
-import drawRelAreaChart from '/plots/stacked_subRel.js';
-import drawEthnonatAreaChart from '/plots/ethnonat_stacked.js';
-import drawLeftAreaChart from '/plots/left_stacked.js';
-import drawRightAreaChart from '/plots/right_stacked.js';
-import drawSingleAreaChart from '/plots/single_stacked.js';
+import drawAreaChart from '{{site.baseurl}}/plots/area_chart.js';
+import drawSunburst from '{{site.baseurl}}/plots/sunburst.js';
+import drawGroupStructures from '{{site.baseurl}}/plots/group_structures.js';
+import drawRecruitment from '{{site.baseurl}}/plots/recruitment.js';
+import drawRelAreaChart from '{{site.baseurl}}/plots/stacked_subRel.js';
+import drawEthnonatAreaChart from '{{site.baseurl}}/plots/ethnonat_stacked.js';
+import drawLeftAreaChart from '{{site.baseurl}}/plots/left_stacked.js';
+import drawRightAreaChart from '{{site.baseurl}}/plots/right_stacked.js';
+import drawSingleAreaChart from '{{site.baseurl}}/plots/single_stacked.js';
 
 
 
