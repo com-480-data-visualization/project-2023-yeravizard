74ba0a33028e16b73cf700a0957dac05a3b0951a
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-05-18 11:44:49 +0200

    m

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import drawAreaChart from '../../plots/area_chart.js';
-import drawSunburst from '../../plots/sunburst.js';
-import drawGroupStructures from '../plots/group_structures.js';
-import drawRecruitment from '../plots/recruitment.js';
-import drawRelAreaChart from '../plots/stacked_subRel.js';
-import drawEthnonatAreaChart from '../../plots/ethnonat_stacked.js';
-import drawLeftAreaChart from '../plots/left_stacked.js';
-import drawRightAreaChart from '../plots/right_stacked.js';
-import drawSingleAreaChart from '../../plots/single_stacked.js';
+import drawAreaChart from './plots/area_chart.js';
+import drawSunburst from 'plots/sunburst.js';
+import drawGroupStructures from 'plots/group_structures.js';
+import drawRecruitment from 'plots/recruitment.js';
+import drawRelAreaChart from 'plots/stacked_subRel.js';
+import drawEthnonatAreaChart from 'plots/ethnonat_stacked.js';
+import drawLeftAreaChart from 'plots/left_stacked.js';
+import drawRightAreaChart from './plots/right_stacked.js';
+import drawSingleAreaChart from './plots/single_stacked.js';
 
 
 
