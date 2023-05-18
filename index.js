60a319cadcf97e574485a02821933b63a4d7e974
SIlvia Romanato <80218776+silvipilvi@users.noreply.github.com>
2023-05-18 10:23:51 +0200

    baseurl

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import drawAreaChart from 'com-480-data-visualization/project-2023-yeravizard/plots/area_chart.js';
-import drawSunburst from 'com-480-data-visualization/project-2023-yeravizard/plots/sunburst.js';
-import drawGroupStructures from 'com-480-data-visualization/project-2023-yeravizard/plots/group_structures.js';
-import drawRecruitment from 'com-480-data-visualization/project-2023-yeravizard/plots/recruitment.js';
-import drawRelAreaChart from 'com-480-data-visualization/project-2023-yeravizard/plots/stacked_subRel.js';
-import drawEthnonatAreaChart from 'com-480-data-visualization/project-2023-yeravizard/plots/ethnonat_stacked.js';
-import drawLeftAreaChart from 'com-480-data-visualization/project-2023-yeravizard/plots/left_stacked.js';
-import drawRightAreaChart from 'com-480-data-visualization/project-2023-yeravizard/plots/right_stacked.js';
-import drawSingleAreaChart from 'com-480-data-visualization/project-2023-yeravizard/plots/single_stacked.js';
+import drawAreaChart from '/plots/area_chart.js';
+import drawSunburst from '/plots/sunburst.js';
+import drawGroupStructures from '/plots/group_structures.js';
+import drawRecruitment from '/plots/recruitment.js';
+import drawRelAreaChart from '/plots/stacked_subRel.js';
+import drawEthnonatAreaChart from '/plots/ethnonat_stacked.js';
+import drawLeftAreaChart from '/plots/left_stacked.js';
+import drawRightAreaChart from '/plots/right_stacked.js';
+import drawSingleAreaChart from '/plots/single_stacked.js';
 
 
 
