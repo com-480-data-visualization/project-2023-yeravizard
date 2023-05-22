7889ca8df8a829c20a9566224082b0ab78538565
AlexanderSternfeld <alex.sternfeld@live.nl>
2023-05-22 14:03:34 +0200

    plot

--- plots/recruitment.js
+++ plots/recruitment.js
@@ -1,74 +1,117 @@
-const drawRecruitment = () => {
-
 // set the dimensions and margins of the graph
-var margin = { top: 10, right: 30, bottom: 30, left: 60 },
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+const margin = { top: 10, right: 150, bottom: 90, left: 60 },
+    width = 800 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
-var svg_recruitment = d3.select("#recruitment_strategies")
+const svg_recruitment = d3.select("#recruitment_strategies")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    .attr("transform",
-        "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
 // Parse the Data
 d3.csv("../data/ideology_recruitment.csv", function (data) {
 
-    // List of subgroups = header of the csv files = soil condition here
-    var subgroups = data.columns.slice(1)
+    // List of subgroups = header of the csv files
+    const subgroups = data.columns.slice(1)
 
     // List of groups = value of the first column called group -> I show them on the X axis
-    var groups = d3.map(data, function (d) { return (d.ideology) }).keys()
+    const groups = data.map(d => d.ideology)
 
     // Add X axis
-    var x = d3.scaleBand()
+    const x = d3.scaleBand()
         .domain(groups)
         .range([0, width])
         .padding([0.2])
     svg_recruitment.append("g")
-        .attr("transform", "translate(0," + height + ")")
+        .attr("transform", `translate(0, ${height})`)
         .call(d3.axisBottom(x).tickSize(0));
 
+    // rotate x axis labels
+    svg_recruitment.selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
     // Add Y axis
-    var y = d3.scaleLinear()
-        .domain([0, 1])
+    const y = d3.scaleLog()
+        .domain([0.001, 1])
         .range([height, 0]);
-    svg_recruitment.append("g")
-        .call(d3.axisLeft(y));
 
     // Another scale for subgroup position?
-    var xSubgroup = d3.scaleBand()
+    const xSubgroup = d3.scaleBand()
         .domain(subgroups)
         .range([0, x.bandwidth()])
         .padding([0.05])
 
     // color palette = one color per subgroup
-    var color = d3.scaleOrdinal()
+    const color = d3.scaleOrdinal()
         .domain(subgroups)
-        .range(['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00'])
+        .range(["#ffffb2", "#fecc5c", "#fd8d3c", "#f03b20", "#bd0026"]);
 
     // Show the bars
     svg_recruitment.append("g")
         .selectAll("g")
         // Enter in data = loop group per group
         .data(data)
-        .enter()
-        .append("g")
-            .attr("transform", function (d) { return "translate(" + x(d.ideology) + ",0)"; })
+        .join("g")
+            .attr("transform", d => `translate(${x(d.ideology)}, 0)`)
         .selectAll("rect")
         .data(function (d) { return subgroups.map(function (key) { return { key: key, value: d[key] }; }); })
-        .enter().append("rect")
-            .attr("x", function (d) { return xSubgroup(d.key); })
-            .attr("y", function (d) { return y(d.value); })
+        .join("rect")
+            .attr("x", d => xSubgroup(d.key))
+            .attr("y", d => y(d.value))
             .attr("width", xSubgroup.bandwidth())
-            .attr("height", function (d) { return height - y(d.value); })
-            .attr("fill", function (d) { return color(d.key); });
+            .attr("height", d => height - y(d.value))
+            .attr("fill", d => color(d.key));
+
+    // Show y axis ticks as 0.0001, 0.001, 0.01 etc. Do not show too many ticks.
+    svg_recruitment.append("g")
+        .call(d3.axisLeft(y)
+            .tickValues([0.001, 0.01, 0.1, 1])
+            // show values as decimal numbers (not scientific notation), but do not show too many digits. Hence 0.0001 is good, but not 1.000 (this should be 1)
+            .tickFormat(d3.format(".4")));
+
+    svg_recruitment.append("text")
+        .attr("class", "y label")
+        // size of text
+        .attr("font-size", "14px")
+        // color black
+        .attr("fill", "black")
+        .attr("text-anchor", "end")
+        .attr("y", 6)
+        // setting below shifts label to the left
+        .attr("dy", "-3em")
+        // shift label to the left
+        .attr("transform", "rotate(-90)")
+        .text("Proportion of groups with the indicated structure");
+
+     legend = svg_recruitment.append("g")
+        .attr("class", "legend")
+        // put legend right from the graph
+        .attr("transform", "translate(" + (width) + "," + 0 + ")")
+        // color black
+        .selectAll("g")
+        .data(color.domain().slice().reverse())
+        .enter().append("g")
+        .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("rect")
+        .attr("x", 0)
+        .attr("width", 18)
+        .attr("height", 18)
+        // color of the rectangles
+        .attr("fill", color);
+
+    legend.append("text")
+        .attr("x", 25)
+        .attr("y", 9)
+        // color black
+        .attr("fill", "black")
+        .attr("dy", ".35em")
+        .style("text-anchor", "start")
+        .text(function (d) { return d; });
 
 })
 
-}
-
-export default drawRecruitment;
